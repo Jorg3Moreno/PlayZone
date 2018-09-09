c8c5224417f087eb27c3496fa8abe7f61e093b46
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2018-09-09 17:30:50 -0500

    added a 'rootReducer' in order to use multiples reducers. 'initialState' is moved to each particular reducer

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,22 +3,23 @@ import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from '../reducers/data';
-import normalizedData from '../schemas/index';
+import reducer from '../reducers/index';
 
-console.log(normalizedData);
-
-const initialState = {
-    data: {
-        entities: normalizedData.entities,
-        categories: normalizedData.result.categories
-    },
-    search: []
-};
+// const initialState = {
+//     data: {
+//         entities: normalizedData.entities,
+//         categories: normalizedData.result.categories,
+//         search: []
+//     },
+//     modal: {
+//         visibility: false,
+//         mediaId: null
+//     }
+// };
 
 const store = createStore(
     reducer,
-    initialState,
+    {},
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import data from './data';
+import modal from './modal';
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    data: data,
+    modal: modal
+});
+
+export default rootReducer;
