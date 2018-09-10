dbd6359debdef646496b105eee88ea68f0abcb6e
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2018-09-09 21:45:21 -0500

    Using external Middleware

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-PlayZone is a web application builded using ReactJS to show videos.
+##PlayZone is a web application builded using ReactJS to show videos.
 
 `npm install --save redux`
 
@@ -7,3 +7,9 @@ PlayZone is a web application builded using ReactJS to show videos.
 `npm install --save normalizr` from [github]('https://github.com/paularmstrong/normalizr/blob/master/docs/api.md')
 
 `npm install --save immutable redux-immutable`
+
+###Middlewares
+
+**Logger:** [github]('https://github.com/evgenyrodionov/redux-logger')
+
+`npm install --save redux-logger`
--- src/entries/home.js
+++ src/entries/home.js
@@ -5,30 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers/index';
 import { Map as map } from 'immutable';
-
-// function logger({ getState, dispatch}) {
-//     return (nextMiddleware) => {
-//         return (action) => {
-//             console.log('old state: ', getState().toJS());
-//             console.log('sending this action: ', action);
-//             const value = nextMiddleware(action);
-//             console.log('new state:', getState().toJS());
-//             return value;
-//         }
-//     }
-// }
-const logger = ({ getState, dispatch}) => nextMiddleware => action => {
-    console.log('old state: ', getState().toJS());
-    console.log('sending this action: ', action);
-    const value = nextMiddleware(action);
-    console.log('new state:', getState().toJS());
-    return value;
-};
+import logger from 'redux-logger';
 
 const store = createStore(
     reducer,
     map(),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     applyMiddleware(logger)
 );
 
